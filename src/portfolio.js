7243a83fa6abd26ae27a15a3a8966facd63e9630
aspolchi <aspolchi@gmail.com>
2022-07-28 12:27:55 -0300

    done

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,48 +1,71 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://asp.ar',
+  title: 'ASP',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Agustín Somoza Polchi',
+  role: 'Front End Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'I am really quick thinker, outside the box type of person, always want to learn new things, very sociable and positive guy.',
+  resume: 'https://drive.google.com/file/d/14x9vvDVeGIZYU9J385qnyuwiT2bS43lp/view?usp=sharing',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/agustin-somoza-polchi/',
+    github: 'https://github.com/aspolchi2',
   },
 }
 
 const projects = [
-  // projects can be added an removed
+  // projects can be added and removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Müssbags',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'E-Commerce made with ReactJs, full responsive using Firebase as a backend ',
+    stack: ['CSS', 'Bootstrap', 'React', 'Firebase'],
+    sourceCode: 'https://github.com/aspolchi2/MB-AgustinSomoza',
+    livePreview: 'https://muss.asp.ar/',
   },
   {
-    name: 'Project 2',
+    name: 'PokeBattle',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Personal project: A game where you have to guess if the Pokémon on the rigth is weaker or stronger',
+    stack: ['SASS', 'Material IU', 'React', 'Pokeapi'],
+    sourceCode: 'https://github.com/aspolchi2/pokebattle',
+    livePreview: 'https://battle.asp.ar/',
   },
   {
-    name: 'Project 3',
+    name: 'Palindrome detector',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Personal project: At an interview someone ask me "How do you detect if a word is a palindrome?" so i made a palindrome detector ',
+    stack: ['SASS', 'Material UI', 'React'],
+    sourceCode: 'https://github.com/aspolchi2/reverse',
+    livePreview: 'https://luzazul.asp.ar/',
+  },
+  {
+    name: 'Los Tilos MH♥',
+    description:
+      'Freelance website: a cabin complex website ',
+    stack: ['SASS', 'Bootstrap', 'HTML5'],
+    livePreview: 'http://lostilosmh.com.ar/',
+  },
+  {
+    name: 'Todo list',
+    description:
+      'Personal project: A todo list to practice the use of React hooks ',
+    stack: ['CSS', 'Material UI', 'React'],
+    sourceCode: 'https://github.com/aspolchi2/dope',
+    livePreview: 'https://www.todo.asp.ar/',
+  },
+  {
+    name: 'Müssbags HTML, CSS',
+    description:
+      'Academy Project: A website where I learn and implemented HTML, CSS, Bootstrap ',
+    stack: ['SASS', 'Bootstrap', 'HTML5'],
+    sourceCode: 'https://github.com/aspolchi2/mussbags',
+    livePreview: 'https://aspolchi2.github.io/mussbags/',
   },
 ]
 
@@ -52,20 +75,18 @@ const skills = [
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
   'SASS',
   'Material UI',
   'Git',
   'CI/CD',
   'Jest',
-  'Enzyme',
+
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'aspolchi@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
