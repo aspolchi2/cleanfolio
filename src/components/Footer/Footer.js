905345d7467c5345fad53d52278c7997a236a11a
aspolchi <aspolchi@gmail.com>
2022-07-28 12:39:58 -0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/aspolchi2/cleanfolio'
       className='link footer__link'
     >
-
+    Thanks
     </a>
   </footer>
 )
